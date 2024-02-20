52ce3b8e4b0cc16963e555bb3eec0cd0c54e0ad9
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-20 20:22:26 +0545

    basics and operators
    basics and operators

---
+++ LEARNINGS/BASIX/first.py
@@ -0,0 +1,92 @@
+# userInp=input("Enter your name: ")
+
+'''
+userInp=int(input("Enter an number: "))
+print(f"You entered: {userInp}")
+'''
+
+'''
+# Changing the value of variable
+a=10
+print(a)
+
+a=44545 # value has been changed
+print(a)
+'''
+'''
+# Assigning multiple values
+a,b,c=1,2,3
+print(a,b,c)
+'''
+'''
+a=b="Hello"
+print(a,b)
+# Ouptut: Hello Hello
+'''
+'''
+val=None
+print(val)
+
+# Ouput: None
+'''
+
+'''
+# Type of data types
+a,b,c,d=1,2.2,True,"Hello"
+e=[1,2.2,True,"HElo"]
+f=(1,2.2,True,"HElo")
+g={"a":"apple", "b":"Ball"}
+h = {1,2.2,True,"HElo"}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+'''
+
+
+'''
+Ouptut:
+<class 'int'>
+<class 'float'>
+<class 'bool'>
+<class 'str'>
+<class 'list'>
+<class 'tuple'>
+<class 'dict'>
+<class 'set'>
+'''
+
+
+# TYPE CONVERSION
+'''
+a=int(input("Enter a number: "))
+b=float(input("Enter a number: "))
+c=str(input("Enter a number: "))
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
+'''
+
+'''
+Ouptut:
+5
+<class 'int'>
+3.0
+<class 'float'>
+2
+<class 'str'>
+'''
+
+# String ConCatenation
+print("Hello " + "World")
---
+++ LEARNINGS/BASIX/operators.py
@@ -0,0 +1,142 @@
+# ARITHMETIC OPERATORS
+'''
+a=7
+b=2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+'''
+
+'''
+Ouptut:
+9
+5
+14
+3.5
+3
+1
+49
+'''
+
+# ASSIGNMENT OPERATORS
+'''
+a=7
+b=2
+
+a+=b # a=a+b
+print(a)
+
+a-=b
+print(a)
+
+a*=b
+print(a)
+
+a/=b
+print(a)
+
+a%=b
+print(a)
+
+a**=b
+print(a)
+'''
+
+# COMPARISION OPERATOR
+'''
+a=7
+b=2
+
+print(a==b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+print(a<b)
+print(a>b)
+'''
+
+'''
+Ouptut:
+False
+True
+False
+True
+False
+True
+'''
+
+# LOGICAL OPS (and or not)
+'''
+print(True and True)
+print(True and False)
+
+print(True or True)
+print(True or False)
+
+print(not True)
+print(not False)
+'''
+
+'''
+Ouptut:
+True
+False
+True
+True
+False
+True
+'''
+
+# BITWISE OPS
+'''
+a=10
+b=4
+
+print(a&b)
+print(a|b)
+print(~a)
+print(a^b)
+print(a<<b)
+print(a>>b)
+'''
+
+'''
+Ouput:
+0
+14
+-11
+14
+160
+0
+'''
+
+# IDENTITY OPS
+'''
+a=10
+b=4
+print(a is b)
+print(a is not b)
+'''
+
+'''
+False
+True
+'''
+
+# MEMBERSHIP OPS
+'''
+l1=[1,2,"a", "krishna"]
+print("krishna" in l1)
+print("krishna" not in l1)
+'''
+
+'''
+Ouptup:
+True
+False
+'''
