3d6e3cc69bbb5ce4c5155ae2a146c7e6eb275e5f
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-21 08:40:21 +0545

    anonymous/lambda function
    anonymous/lambda function

---
+++ LEARNINGS/FUNCTIONS/anonymousFunction.py
@@ -0,0 +1,16 @@
+'''
+def greet(name):
+	return f"Hello, {name}"
+
+name=input("Enter name: ")
+g = greet(name)
+print(g)
+'''
+
+'''
+h=lambda: print("Hello")
+h()
+'''
+
+greet=lambda name: print(f"Hello {name} ")
+greet("Krishna")
