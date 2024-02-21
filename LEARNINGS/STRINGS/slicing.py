5fc95f7c739e8ad2a9d4b41a737d9b937405bf43
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-21 22:23:16 +0545

    Update slicing.py

--- LEARNINGS/STRINGS/slicing.py
+++ LEARNINGS/STRINGS/slicing.py
@@ -5,4 +5,10 @@ a="Hello Caterpillara"
 
 # print(a[2:]) # llo World
 
-print(a[-5:-2]) # lla
+# print(a[-5:-2]) # lla
+
+# print(a[2:-3]) # llo Caterpill
+
+# print(a[::-1]) # arallipretaC olleH
+
+# print("".join(reversed(a))) # arallipretaC olleH
