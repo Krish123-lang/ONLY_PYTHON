6930989f9ffe33039e7c6476c290abc5d7bf5844
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-22 22:34:53 +0545

    Update removingList.py

--- LEARNINGS/LISTS/removingList.py
+++ LEARNINGS/LISTS/removingList.py
@@ -1,4 +1,4 @@
-l1=[1,2,3,4,5]
+l1=[1,2,3,4,"ohio",5]
 # using pop() function
 '''
 #l1.pop() # [1, 2, 3, 4]
@@ -9,7 +9,20 @@ print(l1)
 # Using remove() function => removes the given element
 '''
 #l1.remove(3) # [1, 2, 4, 5]
-l1.remove(2) # [1, 3, 4, 5]
+#l1.remove(2) # [1, 3, 4, 5]
+#l1.remove("ohio") # [1, 2, 3, 4, 5]
 print(l1)
 '''
 
+#using del keyword
+'''
+# del l1[0] # [2, 3, 4, 'ohio', 5] => removing first element
+del l1 # deletes entire list including the l1 variable
+print(l1)
+'''
+
+# using clear function to empty a list
+'''
+l1.clear() # []
+print(l1)
+'''
