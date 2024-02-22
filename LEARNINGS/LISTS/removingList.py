220355006091f3820ecd4e4d0f33218be86e5457
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-22 21:46:39 +0545

    removing list elements
    removing list elements

---
+++ LEARNINGS/LISTS/removingList.py
@@ -0,0 +1,15 @@
+l1=[1,2,3,4,5]
+# using pop() function
+'''
+#l1.pop() # [1, 2, 3, 4]
+l1.pop(3) # [1, 2, 3, 5] => removes the 3rd position of element i.e. 4
+print(l1)
+'''
+
+# Using remove() function => removes the given element
+'''
+#l1.remove(3) # [1, 2, 4, 5]
+l1.remove(2) # [1, 3, 4, 5]
+print(l1)
+'''
+
