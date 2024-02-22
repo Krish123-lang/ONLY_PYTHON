0b1e4d9f8f96f5d2a572f590b46822614be82c95
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-22 21:52:40 +0545

    list comprehension
    list comprehension

---
+++ LEARNINGS/LISTS/comprehension.py
@@ -0,0 +1,20 @@
+'''
+Syntax:
+newList = [ expression(element) for element in oldList if condition ]
+'''
+
+'''
+l1=[]
+
+for i in range(1,11):
+    if i%2==0:
+        l1.append(i)
+
+print(l1)
+'''
+
+'''
+odd_square = [x ** 2 for x in range(1, 11) if x % 2 == 1] # if x % 2 == 0 for even number
+print(odd_square) # [4, 16, 36, 64, 100]
+'''
+
