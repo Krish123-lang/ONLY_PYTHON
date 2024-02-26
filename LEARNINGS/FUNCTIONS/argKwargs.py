7ab6b5bfd1b5db66a82e9998b07885b99911bf22
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-26 09:57:32 +0545

    Create argKwargs.py

---
+++ LEARNINGS/FUNCTIONS/argKwargs.py
@@ -0,0 +1,6 @@
+def hello(*args, **kwargs):
+    print("args: ", args)
+    print("kwargs: ", kwargs)
+
+
+hello("this", 'is', 'krishna', 'the', 'dumb', 'machine', first="hello", second='programmers', third='this', fourth='is', fifth='krishna')
