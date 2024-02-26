e9d344990ccc87db0aa8490c92e7b2ea58818342
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-26 09:33:52 +0545

    args

---
+++ LEARNINGS/FUNCTIONS/args.py
@@ -0,0 +1,47 @@
+'''
+def greet(name):
+    return f"Hello, {name}"
+
+g=greet("Krishna")
+print(g)
+'''
+# *******************************************************************
+# ARGS
+
+'''
+def hello(*argv):
+    for arg in argv:
+        print(arg)
+
+
+hello("Hello", "this", "is", "Krishna")
+'''
+
+'''
+Hello
+this
+is
+Krishna
+'''
+# *******************************************************************
+
+'''
+def hello(arg1, *argv):
+    print(f"first argument: {arg1}\n")
+
+    for arg in argv:
+        print(f"Argv* : {arg}")
+
+
+hello("This", "is", "krishna", "mandal")
+'''
+
+'''
+first argument: This
+
+Argv* : is
+Argv* : krishna
+Argv* : mandal
+'''
+# ******************************************************************
+
