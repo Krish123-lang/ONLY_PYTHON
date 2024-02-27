b9a08fd563ea0240ca4ee6f4dfe9595c8c3e8ba1
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-27 09:24:47 +0545

    Create stringModify.py

---
+++ LEARNINGS/STRINGS/stringModify.py
@@ -0,0 +1,8 @@
+a="Python Programming"
+b="     Python Programming     "
+
+#print(a.lower()) #python programming
+#print(a.upper()) #PYTHON PROGRAMMING
+#print(b.strip()) # Python Programming => removes extra whitespaces
+#print(a.replace("o", "i")) #Pythin Prigramming => replaces all occurences
+print(a.split(" ")) #['Python', 'Programming'] => splits them and converts into list
