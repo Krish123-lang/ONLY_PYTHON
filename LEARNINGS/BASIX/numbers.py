521063d5a4e095b222a6e37b056728b7339e923e
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-27 08:52:01 +0545

    Create numbers.py

---
+++ LEARNINGS/BASIX/numbers.py
@@ -0,0 +1,21 @@
+a=20
+b=3
+
+pow1=2
+pow2=5
+
+# abs() for absolute value
+#print(b-a) # -17
+#print(abs(b-a)) # 17
+
+# divmod() prints quotient and remainder simultaneously 
+#print(divmod(a,b)) # (6, 2)
+
+# prints power 2^5
+#print(pow(pow1,pow2)) #32
+
+#print(a/b)# 6.666666666666667
+#print(round(a/b)) # 7
+
+l1=[1,2,3,4,5,6,7,8,9,10]
+print(sum(l1)) # 55
