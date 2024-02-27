5517e76e27a497274296bd091f6d9aa820fe0e50
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-27 09:09:32 +0545

    Create basixString.py

---
+++ LEARNINGS/STRINGS/basixString.py
@@ -0,0 +1,33 @@
+a="Python Programming"
+#print(len(a)) # 18
+
+#print("Python" in a) # True
+
+# looping 
+'''
+for i in a:
+    print(i)
+'''
+
+'''
+P
+y
+t
+h
+o
+n
+
+P
+r
+o
+g
+r
+a
+m
+m
+i
+n
+g
+'''
+
+print(a[0:3]) # Pyt
