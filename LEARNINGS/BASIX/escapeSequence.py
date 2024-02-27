1996a83ba7cd5513abeb0c3ce98c2954fbcb0e51
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-27 08:51:52 +0545

    Create escapeSequence.py

---
+++ LEARNINGS/BASIX/escapeSequence.py
@@ -0,0 +1,31 @@
+# prints single quote '
+txt="Python \'Programming" # #Python 'Programming
+
+# prints double quote "
+txt="Python \"Programming" # Python "Programming
+
+# prints \ 
+txt="Python \\Programming" # Python \Programming
+
+# prints in new line
+txt="Python \nProgramming" 
+'''
+Python
+Programming
+'''
+
+# prints the text after \r
+txt="Python \rProgramming" # Programming
+
+# Adds a tab between texts
+txt="Python \tProgramming" # Python  Programming
+
+# adds backspace i.e. remove space 
+txt="Python \bProgramming" # PythonProgramming
+
+# form feed. I don't know what it is.
+txt="Python \fProgramming" # Python ♀Programming
+
+print(txt) 
+
+
