d68fc327f16338230503782bb4f1e8d331ca765c
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-27 22:08:50 +0545

    string capitalize, case fold, center

---
+++ LEARNINGS/STRINGS/stringMethods/stringMethods.py
@@ -0,0 +1,18 @@
+a="python programming"
+
+#print(a.capitalize()) # Python programming => capitalizes only the first occurences
+
+#print(a.casefold()) #makes the string lowercase
+
+'''
+txt = "banana"
+x = txt.center(20)
+print(x) #       banana
+'''
+
+'''
+txt = "banana"
+x = txt.center(20, '0')
+print(x) #0000000banana0000000
+'''
+
