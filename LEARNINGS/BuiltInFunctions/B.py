64804208752dc90d762a9c6de7d6dd980a738939
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-09 14:47:10 +0545

    built in functions with B

---
+++ LEARNINGS/BuiltInFunctions/B.py
@@ -0,0 +1,23 @@
+# bin() => Returns the binary version of a number
+
+# print(bin(13)) #0b1101
+# *********************************************************************
+
+# bool() => Returns the boolean value of the specified object
+'''
+print(bool(0)) # False
+print(bool(4)) # True
+print(bool("Apple")) # True
+'''
+# *********************************************************************
+
+# bytearray() => Returns an array of bytes
+# print(bytearray(4)) # bytearray(b'\x00\x00\x00\x00')
+
+# *********************************************************************
+
+# bytes() => Returns a bytes object
+# print(bytes(4)) # b'\x00\x00\x00\x00'
+
+# *********************************************************************
+
