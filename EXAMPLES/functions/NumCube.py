1b2506511b1704100d4a0c5accd0cd8b74dc969a
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-09 11:57:05 +0545

    sum and cube

---
+++ EXAMPLES/functions/NumCube.py
@@ -0,0 +1,13 @@
+def cube(number):
+    return number*number*number
+
+
+number = int(input("enter a number: "))
+num = cube(number)
+
+print(f"the cube of {number} is {num}")
+
+'''
+enter a number: 2
+the cube of 2 is 8
+'''
---
+++ EXAMPLES/functions/first.py
@@ -0,0 +1,14 @@
+def sum(a, b):
+    return a+b
+
+
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+sum = sum(a, b)
+print(f"Sum: {sum}")
+
+'''
+enter a: 4
+enter b: 3
+Sum: 7
+'''
