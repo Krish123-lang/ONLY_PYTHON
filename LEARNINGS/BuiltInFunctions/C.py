e51a15afd215f13cbcf1f608975e92509c1da71a
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-09 14:58:21 +0545

    Create C.py

---
+++ LEARNINGS/BuiltInFunctions/C.py
@@ -0,0 +1,19 @@
+# callable() => Returns True if the specified object is callable, otherwise False
+
+# print(callable(3)) # False
+# ****************************************************************************
+
+# chr() => Returns a character from the specified Unicode code
+'''
+for i in range(0, 100):
+    print(chr(i))
+'''
+# ****************************************************************************
+
+# complex() => Returns a complex number
+'''
+print(complex(3,2)) #(3+2j)
+print(complex(3+2j)) # (3+2j)
+'''
+
+# ****************************************************************************
