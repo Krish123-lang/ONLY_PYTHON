186beb7a832badc975c4f92c2d4748e2133b7202
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-14 20:01:20 +0545

    basic python functions

---
+++ LEARNINGS/functions/first.py
@@ -0,0 +1,19 @@
+'''def greet(name):
+    return f"Hello, {name}"
+
+
+name = input("Enter your name: ")
+n = greet(name)
+print(n)
+'''
+
+
+def sum(a, b):
+    return f"The sum of {a} and {b} is: {a+b}"
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+a = sum(a, b)
+print(a)
