2c7abd7256aa10052be26fabe9430dc030232f2c
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-22 18:54:02 +0545

    Return multiple values from a function

---
+++ LEARNINGS/pyNative/PyTricks/returnMultipleValFromFunc.py
@@ -0,0 +1,8 @@
+def multiplication_Division(num1, num2):
+    return num1*num2, num2/num1
+
+
+product, division = multiplication_Division(10, 20)
+print("Product", product, "Division", division)
+
+# Product 200 Division 2.0
