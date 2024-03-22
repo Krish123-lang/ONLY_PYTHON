2deddd307cc35cfe94dd8b4a9ae58972d0e0fcd1
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-22 18:34:55 +0545

    memory used by objects

---
+++ LEARNINGS/pyNative/PyTricks/memObj.py
@@ -0,0 +1,12 @@
+import sys
+
+list1 = ['Scott', 'Eric', 'Kelly', 'Emma', 'Smith']
+print("size of list = ", sys.getsizeof(list1))
+
+name = 'pynative.com'
+print("size of name = ", sys.getsizeof(name))
+
+'''
+size of list =  104
+size of name =  53
+'''
