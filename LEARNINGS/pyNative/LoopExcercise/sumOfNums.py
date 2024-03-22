2d232fe654a38bd2e7441dc1461bf906c2986416
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-22 09:40:16 +0545

    sum of numbers upto given numbers

---
+++ LEARNINGS/pyNative/LoopExcercise/sumOfNums.py
@@ -0,0 +1,14 @@
+num = int(input("enter the number: "))
+sum = 0
+
+for i in range(1, num+1):
+    sum += i
+
+print(f"the sum is: {sum}")
+
+# 1+2+3+4+5
+
+'''
+enter the number: 5
+the sum is: 15
+'''
