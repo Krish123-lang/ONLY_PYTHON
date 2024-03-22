cd2e3c5ae98e3d6cf580d7d9262ee9e1252c34c8
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-22 18:28:04 +0545

    pattern and time by code

---
+++ LEARNINGS/pyNative/PyTricks/timeCode.py
@@ -0,0 +1,9 @@
+import time
+startTime = time.time()
+
+endTime = time.time()
+
+totalTime = endTime-startTime
+print(f"Total time required to execute code is : {totalTime}")
+
+# Total time required to execute code is : 3.814697265625e-06
