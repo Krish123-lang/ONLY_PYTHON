264814ad1bf94c596941bf9ca358da4d8393d864
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-22 09:44:24 +0545

    Update sumOfNums.py

--- LEARNINGS/pyNative/LoopExcercise/sumOfNums.py
+++ LEARNINGS/pyNative/LoopExcercise/sumOfNums.py
@@ -1,14 +1,26 @@
 num = int(input("enter the number: "))
+
+'''
 sum = 0
 
 for i in range(1, num+1):
     sum += i
 
 print(f"the sum is: {sum}")
-
+'''
 # 1+2+3+4+5
 
 '''
 enter the number: 5
 the sum is: 15
+'''
+# ---------------------------
+
+
+x = sum(range(1, num+1))
+print(f"the x is: {x}")
+
+'''
+enter the number: 5
+the x is: 15
 '''
