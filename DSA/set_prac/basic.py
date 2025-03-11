d91f740d7b0e84123c91f27a41421f61a350ac3d
krishna <mandalkrishna739@gmail.com>
2025-03-11 17:47:25 +0545

    Set python Practice

---
+++ DSA/set_prac/basic.py
@@ -0,0 +1,40 @@
+set1 = {1, 2, 2, 3, 4, 5, 5}
+# print(set1) # {1, 2, 3, 4, 5}
+
+# list_set=list(set1)
+# print(list_set[3]) # 4
+
+'''
+s1 = set('krishna is awesome')
+print(s1)
+'''
+# for i in set1:
+#     print(i)
+
+'''
+1
+2
+3
+4
+5
+'''
+
+s2 = {5, 4, 7, 3, 9, 1}
+# s2.update([45, 89])
+# s2.add(90)
+# print(s2) # {1, 3, 4, 5, 7, 9}
+# print(s2)
+
+# s2.remove(7) # it removes 7 (element from the set)
+
+# s2.discard(3) # it removes 3 (element from the set)
+
+# s2.clear() # clears the entire set
+
+# s2.pop()
+
+# print(s2)
+
+res = [(x, y) for x in range(3) for y in range(3)]
+print(res)
+# [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
